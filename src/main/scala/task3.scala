0714e6eb8b3626f58a5f59831557955bb6b771b5
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2024-03-14 12:58:03 +0100

    WIP

--- src/main/scala/task3.scala
+++ src/main/scala/task3.scala
@@ -1,5 +1,10 @@
 object Task3:
+    @annotation.tailrec
     def gcd(a: Int, b: Int): Int =
+        println(s"${a}, ${b}")
         b match
+            case a if a == 0 => throw java.lang.IllegalArgumentException()
+            case a if a == b => a
+            case a if a <= b => gcd(b, a)
             case b if b != 0 => gcd(b, a % b)
             case _ => a
--- src/test/scala/task3.scala
+++ src/test/scala/task3.scala
@@ -6,4 +6,5 @@ class Task3Test:
     @Test def testGCD() =
         assertEquals(4, Task3.gcd(12, 8))
         assertEquals(7, Task3.gcd(14, 7))
+        assertEquals(7, Task3.gcd(0, 7))
     
